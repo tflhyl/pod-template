ffe30f576a161a9b2a1637d65a9dd41b4b4577fd
Theodore Felix Leo <tflhyl@gmail.com>
2016-03-15 16:48:29 +0800

    Remove file header comments and storyboard

--- templates/ios/Example/PROJECT/CPDViewController.h
+++ templates/ios/Example/PROJECT/CPDViewController.h
@@ -1,11 +1,3 @@
-//
-//  CPDViewController.h
-//  PROJECT
-//
-//  Created by PROJECT_OWNER on TODAYS_DATE.
-//  Copyright (c) TODAYS_YEAR PROJECT_OWNER. All rights reserved.
-//
-
 @import UIKit;
 
 @interface CPDViewController : UIViewController
--- templates/ios/Example/PROJECT/main.m
+++ templates/ios/Example/PROJECT/main.m
@@ -1,16 +1,8 @@
-//
-//  main.m
-//  PROJECT
-//
-//  Created by PROJECT_OWNER on TODAYS_DATE.
-//  Copyright (c) TODAYS_YEAR PROJECT_OWNER. All rights reserved.
-//
-
 @import UIKit;
+
 #import "CPDAppDelegate.h"
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char * argv[]) {
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([CPDAppDelegate class]));
     }
